d3beb8f3f3e9c023a9dfb9a2555fb2030ea457c6
Savan Ankola <38508703+savanankola@users.noreply.github.com>
2019-05-11 11:57:10 +0530

    Add files via upload

---
+++ MotionEffect/MotionEffect/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  MotionEffect
+//
+//  Created by Training on 15/08/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        applyMotionEffect(toView: backgroundImageView, magnitude: 40)
+        applyMotionEffect(toView: logoImageView, magnitude: -40)
+        
+    }
+    
+    
+    
+    func applyMotionEffect (toView view:UIView, magnitude:Float) {
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+        xMotion.minimumRelativeValue = -magnitude
+        xMotion.maximumRelativeValue = magnitude
+        
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+        yMotion.minimumRelativeValue = -magnitude
+        yMotion.maximumRelativeValue = magnitude
+        
+        let group = UIMotionEffectGroup()
+        group.motionEffects = [xMotion, yMotion]
+        
+        view.addMotionEffect(group)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
